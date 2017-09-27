37774bb83949d685483cf4ebd7ff06f2620a22ef
Diego Dias Ribeiro da Silva <diego.silva@easynvest.com.br>
2017-09-27 15:17:14 -0300

    Cria arquivo index.js

--- src/app.js
+++ src/app.js
@@ -31,4 +31,9 @@ app.use('/data-import', dataImportRoute)
 app.use('/ipca', ipcaRoute);
 app.use('/cdi', cdiRoute);
 
-module.exports = app;
+app.set('port', (process.env.PORT || 3000))
+app.listen(app.get('port'), function(){
+  console.log('Server listening on port: ', app.get('port'))
+});
+
+// module.exports = app;
